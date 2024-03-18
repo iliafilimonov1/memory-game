190d77b652b66d4a61e96e98c90e40caa776280f
Ilia Filimonov <9991571155i@gmail.com>
2024-03-19 00:50:34 +0700

    [MEMORY-GAME]-001. Получение игровых селекторов. Создание базового стейта. Функция генерации игрового поля (WIP).

--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,75 @@
-// your code
-// ["🥔", "🍒", "🥑", "🌽", "🥕", "🍇", "🍉", "🍌", "🥭", "🍍"]
+const EMOJIS = [
+  "🥑",
+  "🍇",
+  "🍒",
+  "🌽",
+  "🥕",
+  "🍉",
+  "🥔",
+  "🍌",
+  "🥭",
+  "🍍",
+  "🥕",
+  "🍉",
+  "🥔",
+  "🍌",
+  "🥭",
+  "🍍",
+];
+
+/**
+ * Состояние игры
+ * @property {boolean} isGameStarted - Игра началась или нет.
+ * @property {number} totalTime - Общее время в игре.
+ * @property {number} flippedCards - Количество перевернутых карточек.
+ * @property {number} totalFlips - Общее количество перевернутых карточек.
+ */
+const STATE = {
+  isGameStarted: false,
+  totalTime: 0,
+  flippedCards: 0,
+  totalFlips: 0,
+};
+
+/**
+ * Контролы игры
+ * @property {HTMLDivElement} boardContainer - Контейнер игрового поля.
+ * @property {HTMLDivElement} board - Основное содержимое поля (4x4).
+ * @property {HTMLDivElement} moves - Контрол для учета шагов.
+ * @property {HTMLDivElement} timer - Контрол для учета времени.
+ * @property {HTMLButtonElement} start - Кнопка для старта игры.
+ */
+const SELECTORS = {
+  boardContainer: document.querySelector(".board-container"),
+  board: document.querySelector(".board"),
+  moves: document.querySelector(".moves"),
+  timer: document.querySelector(".timer"),
+  start: document.querySelector("button"),
+};
+
+/**
+ * Генерация игрового поля
+ */
+const generateGame = () => {
+  // Получение data атрибута
+  const dimensions = SELECTORS.board.dataset.dimension;
+
+  if (dimensions % 2 !== 0) {
+    throw new Error("Размер игрового поля должен быть четным!");
+  }
+
+  // Итерация по карточкам
+  const cardsHTML = EMOJIS.map((emoji) => {
+    return `
+        <div class="card">
+            <div class="card-front"></div>
+            <div class="card-back">${emoji}</div>
+        </div>
+    `;
+  }).join("");
+
+  // Вставка карточек в игровое поле
+  SELECTORS.board.insertAdjacentHTML("beforeend", cardsHTML);
+};
+
+generateGame();
