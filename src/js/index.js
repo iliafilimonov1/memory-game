bb070dacc69b0dd90a5888724a9e88193b4ca935
Ilia Filimonov <9991571155i@gmail.com>
2024-03-21 00:01:29 +0700

    [MEMORY-GAME]-001. Функция перемешивания и получения случайных карточек.

--- src/js/index.js
+++ src/js/index.js
@@ -1,21 +1,25 @@
-const EMOJIS = [
-  "🥑",
-  "🍇",
-  "🍒",
-  "🌽",
-  "🥕",
-  "🍉",
-  "🥔",
-  "🍌",
-  "🥭",
-  "🍍",
-  "🥕",
-  "🍉",
-  "🥔",
-  "🍌",
-  "🥭",
-  "🍍",
-];
+const EMOJIS = ["🥑", "🍇", "🍒", "🌽", "🥕", "🍉", "🥔", "🍌", "🥭", "🍍"];
+
+/**
+ *
+ * @param {strings[]} items - Абстрактные данные для перемешивания и сортировки.
+ * @returns {strings[]} - Перемешанный массив с данными.
+ */
+function shuffleAndPickRandom(items) {
+  // сортировка исходного массива в случайном порядке
+  const sortedArr = items.sort(() => Math.random(items) - 0.5);
+
+  // достаем из 10 элементов первые 8
+  const dublicateArr = [...sortedArr].slice(0, 8);
+
+  // из массива в 8 элементов, делаем 16
+  const doubleArr = [...dublicateArr, ...dublicateArr];
+
+  // сортировка массива из 16 элементов в случайном порядке
+  const sortedDoubleArr = doubleArr.sort(() => Math.random(doubleArr) - 0.5);
+
+  return sortedDoubleArr;
+}
 
 /**
  * Состояние игры
